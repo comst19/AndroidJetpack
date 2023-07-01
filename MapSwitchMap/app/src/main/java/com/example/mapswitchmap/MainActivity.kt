2caafea8d68d5e0b60a8054f339b15de70e53189
Minsu <comst19@naver.com>
2023-07-01 21:02:31 +0900

    ViewModel에서 LiveData 값 변경(Map,SwitchMap)

---
+++ MapSwitchMap/app/src/main/java/com/example/mapswitchmap/MainActivity.kt
@@ -0,0 +1,35 @@
+package com.example.mapswitchmap
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import com.example.mapswitchmap.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
+    private lateinit var viewModel: MainViewModel
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
+
+        viewModel.mapLiveData.observe(this, Observer {
+            binding.resultArea1.text = it.toString()
+        })
+
+        viewModel.switchMapLiveData.observe(this, Observer {
+            binding.resultArea2.text = it.toString()
+        })
+
+        binding.btnArea.setOnClickListener {
+            val count = binding.inputArea.text.toString().toInt()
+            viewModel.setLiveDataValue(count)
+        }
+    }
+}
